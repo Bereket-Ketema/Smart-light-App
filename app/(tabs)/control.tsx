b82a0b4f784ca48d85dec4cb9f5f3d0c5a5f6c15
Bereket-Ketema <bereketketema866@gmail.com>
2026-04-16 00:38:06 +0300

    control page have been changed from class to function

--- app/(tabs)/control.tsx
+++ app/(tabs)/control.tsx
@@ -1,15 +1,13 @@
 import { Text, View, StyleSheet } from 'react-native'
-import React, { Component } from 'react'
 
-export default class control extends Component {
-  render() {
-    return (
+export default function Control() {
+  return (
       <View style={styles.container}>
         <Text>control</Text>
       </View>
     )
   }
-}
+
 
 
 const styles = StyleSheet.create({
